5098ffa46add4a4f9dc98f91b8a05eb93ae56d20
Jakub Kołuda <jko01@outlook.com>
2023-09-21 19:57:38 +0200

    Add login form.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,71 @@
+"use client";
+import { Button, Checkbox, Form, Input } from "antd";
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <Form
+      name="basic"
+      labelCol={{
+        span: 8,
+      }}
+      wrapperCol={{
+        span: 16,
+      }}
+      style={{
+        maxWidth: 600,
+      }}
+      initialValues={{
+        remember: true,
+      }}
+      autoComplete="off"
+    >
+      <Form.Item
+        label="Email"
+        name="Email"
+        rules={[
+          {
+            required: true,
+            message: "Please enter your email!",
+          },
+        ]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[
+          {
+            required: true,
+            message: "Please enter your password!",
+          },
+        ]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item
+        wrapperCol={{
+          offset: 8,
+          span: 16,
+        }}
+      >
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
 }
