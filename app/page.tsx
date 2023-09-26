4f622a49858cb650f67fb5edb6762819f6ae736f
Jakub Kołuda <jko01@outlook.com>
2023-09-26 21:16:02 +0200

    Change styling.

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,14 @@ import { Button, Form, Input } from "antd";
 import { useRouter } from "next/navigation";
 import useSignIn from "@/app/hooks/useSignIn";
 import { Typography } from "antd";
+import styled from "@emotion/styled";
 
 interface FormValues {
   email: string;
   password: string;
 }
 
-const { Paragraph } = Typography;
+const { Paragraph, Title } = Typography;
 
 export default function Page() {
   const router = useRouter();
@@ -21,56 +22,72 @@ export default function Page() {
     if (valid) router.push("/dashboard");
   };
 
-  return (
-    <Form
-      name="basic"
-      layout="vertical"
-      style={{
-        maxWidth: 400,
-      }}
-      initialValues={{
-        email: "budget@example.com",
-        password: "Demo123",
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[
-          {
-            required: true,
-            message: "Please enter your email!",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
+  const FormStyled = styled(Form)`
+    margin: 0 auto;
+    max-width: 400px;
+  `;
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please enter your password!",
-          },
-        ]}
+  const PStyled = styled(Paragraph)`
+    &.ant-typography-danger {
+      text-align: center;
+    }
+  `;
+
+  const TitleStyled = styled(Title)`
+    &.ant-typography {
+      margin: 5rem 0;
+      text-align: center;
+    }
+  `;
+
+  return (
+    <main>
+      <TitleStyled>Budget Planner</TitleStyled>
+      <FormStyled
+        name="basic"
+        layout="vertical"
+        initialValues={{
+          email: "budget@example.com",
+          password: "Demo123",
+        }}
+        onFinish={onFinish}
       >
-        <Input.Password />
-      </Form.Item>
-      {isError && (
-        <Form.Item>
-          <Paragraph type="danger" style={{ textAlign: "center" }}>
-            Wrong credentials!
-          </Paragraph>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[
+            {
+              required: true,
+              message: "Please enter your email!",
+            },
+          ]}
+        >
+          <Input />
         </Form.Item>
-      )}
-      <Form.Item>
-        <Button type="primary" htmlType="submit" disabled={isLoading} block>
-          {isLoading ? "Signing in..." : "Sign in"}
-        </Button>
-      </Form.Item>
-    </Form>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please enter your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+        {isError && (
+          <Form.Item>
+            <PStyled type="danger">Wrong credentials!</PStyled>
+          </Form.Item>
+        )}
+        <Form.Item>
+          <Button type="primary" htmlType="submit" disabled={isLoading} block>
+            {isLoading ? "Signing in..." : "Sign in"}
+          </Button>
+        </Form.Item>
+      </FormStyled>
+    </main>
   );
 }
