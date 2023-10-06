5768f9164a659ca69e66c65834200576f82be19d
Jakub Kołuda <jko01@outlook.com>
2023-10-06 19:28:56 +0200

    Change date picker. Add arrows that change date. Add gray color to theme.

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,6 +5,8 @@ import type { DatePickerProps } from "antd";
 import { Typography, DatePicker } from "antd";
 import styled from "@emotion/styled";
 import dayjs from "dayjs";
+import { useState } from "react";
+import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 
 const { Text, Title } = Typography;
 
@@ -26,18 +28,39 @@ const DivStyled = styled.div`
   border-bottom: 1px solid;
   border-bottom-color: ${(props) => props.theme["gray-5"]};
 `;
-
-const DatePickerStyled = styled(DatePicker)`
+const DateContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
   margin-top: 1rem;
+`;
+const LeftOutlinedStyled = styled(LeftOutlined)`
+  margin-left: 0.5rem;
+  padding: 0.5rem;
+  border: 1px solid;
+  border-color: ${(props) => props.theme["gray-6"]};
+`;
+const RightOutlinedStyled = styled(RightOutlined)`
+  margin-left: 0.5rem;
+  padding: 0.5rem;
+  border: 1px solid;
+  border-color: ${(props) => props.theme["gray-6"]};
 `;
 
 const monthFormat = "MMMM YYYY";
 
 export default function BudgetPage() {
   const { user, userIsLoading } = useUser();
+  const [month, setMonth] = useState(dayjs());
 
-  const monthChange: DatePickerProps["onChange"] = (date, dateString) => {
-    console.log(date, dateString);
+  const monthChange: DatePickerProps["onChange"] = (date) => {
+    setMonth(date);
+  };
+
+  const monthHandle = (action: string) => {
+    action === "increase"
+      ? setMonth((prevState) => prevState.add(1, "month"))
+      : setMonth((prevState) => prevState.subtract(1, "month"));
   };
 
   if (!user && !userIsLoading) {
@@ -50,12 +73,18 @@ export default function BudgetPage() {
         <TitleStyled>Budget</TitleStyled>
         <TextStyled>1520 to be assigned</TextStyled>
       </DivStyled>
-      <DatePickerStyled
-        defaultValue={dayjs()}
-        onChange={monthChange}
-        picker="month"
-        format={monthFormat}
-      />
+      <DateContainer>
+        <DatePicker
+          value={month}
+          onChange={monthChange}
+          picker="month"
+          format={monthFormat}
+        />
+        <div>
+          <LeftOutlinedStyled onClick={() => monthHandle("decrease")} />
+          <RightOutlinedStyled onClick={() => monthHandle("increase")} />
+        </div>
+      </DateContainer>
     </>
   );
 }
--- app/providers/ThemeProvider.tsx
+++ app/providers/ThemeProvider.tsx
@@ -6,6 +6,18 @@ const { useToken } = theme;
 
 export function EmotionProvider({ children }) {
   const { token } = useToken();
+  token["gray-1"] = "#ffffff";
+  token["gray-2"] = "#fafafa";
+  token["gray-3"] = "#f5f5f5";
+  token["gray-4"] = "#f0f0f0";
   token["gray-5"] = "#d9d9d9";
+  token["gray-6"] = "#bfbfbf";
+  token["gray-7"] = "#8c8c8c";
+  token["gray-8"] = "#595959";
+  token["gray-9"] = "#434343";
+  token["gray-10"] = "#262626";
+  token["gray-11"] = "#1f1f1f";
+  token["gray-12"] = "#141414";
+  token["gray-13"] = "#000000";
   return <ThemeProvider theme={token}>{children}</ThemeProvider>;
 }
